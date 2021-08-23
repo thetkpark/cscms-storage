e3e86b4da207a840f32dd4499f3e41a3a322a0b6
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-08-23 22:36:29 +0700

    Use storeDuration

--- cmd/cleaner/main.go
+++ cmd/cleaner/main.go
@@ -9,13 +9,14 @@ import (
 	"gorm.io/gorm"
 	"log"
 	"os"
+	"strconv"
 	"time"
 )
 
 func main() {
 	logger := hclog.Default()
 
-	storagePath, sqlitePath := getEnv()
+	storagePath, sqlitePath, storeDuration := getEnv()
 	isFailed := false
 	// Open data store
 	db, err := gorm.Open(sqlite.Open(sqlitePath), &gorm.Config{})
@@ -44,7 +45,7 @@ func main() {
 		}
 
 		// Check if not expire
-		if fileInfo.CreatedAt.UTC().Add(time.Hour * 24 * 30).After(time.Now().UTC()) {
+		if fileInfo.CreatedAt.UTC().Add(storeDuration).After(time.Now().UTC()) {
 			// Not expire -> continue
 			continue
 		}
@@ -66,11 +67,23 @@ func main() {
 	logger.Info(fmt.Sprintf("Delete %d file", deletedCount))
 }
 
-func getEnv() (string, string) {
+func getEnv() (string, string, time.Duration) {
 	storagePath := os.Getenv("STORAGE_PATH")
 	dbPath := os.Getenv("SQLITE_PATH")
 	if len(storagePath) == 0 || len(dbPath) == 0 {
 		log.Fatalln("SQLITE_PATH STORAGE_PATH env must be defined")
 	}
-	return storagePath, dbPath
+
+	// optional env
+	storeDuration := os.Getenv("STORE_DURATION") // in days
+	duration := time.Hour * 24 * 30
+	if len(storeDuration) != 0 {
+		date, err := strconv.Atoi(storeDuration)
+		if err != nil {
+			log.Fatalln("STORE_DURATION is not a valid number")
+		}
+		duration = time.Hour * 24 * time.Duration(date)
+	}
+
+	return storagePath, dbPath, duration
 }

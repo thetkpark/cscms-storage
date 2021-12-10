b4eb8fe00cf9a79c51de9b0b39df52cf204d9d57
GGolfz <wisarutgolf11@gmail.com>
2021-12-10 21:03:52 +0700

    add: Sidebar :sparkles:

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,30 @@
+import Icon from './Icon'
+import { Fragment } from 'react'
+import styles from '../styles/Sidebar.module.css'
+const Sidebar = ({ currentRoute = 'file', handleChangeRoute }) => {
+	const routes = ['file', 'image']
+	const activeColor = '#6F6CBB'
+	const inactiveColor = '#B0B2E2'
+	return (
+		<Fragment>
+			<div className={styles.SideBar}>
+				{routes.map(route => (
+					<div
+						className={`${styles.SideBar__item} ${
+							currentRoute === route ? styles.active : ''
+						}`}
+						key={route}
+                        onClick={() => handleChangeRoute(route)}
+					>
+						<Icon
+							name={route}
+							color={currentRoute === route ? activeColor : inactiveColor}
+						/>
+					</div>
+				))}
+			</div>
+		</Fragment>
+	)
+}
+
+export default Sidebar

b821ec02dd1e8977fd5527e98b7d1f03c39bd05f
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-12-11 10:27:39 +0700

    add original filename

--- data/model/image.go
+++ data/model/image.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type Image struct {
-	ID        uint      `gorm:"primaryKey,autoIncrement" json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Filename  string    `json:"filename"`
-	FileSize  uint64    `json:"file_size"`
-	FilePath  string    `json:"file_path"`
-	Token     string    `json:"token"`
-	UserID    uint
+	ID               uint      `gorm:"primaryKey,autoIncrement" json:"id"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
+	OriginalFilename string    `json:"original_filename"`
+	FileSize         uint64    `json:"file_size"`
+	FilePath         string    `json:"file_path"`
+	Token            string    `json:"token"`
+	UserID           uint
 }

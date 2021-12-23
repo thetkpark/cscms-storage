9b7db1f9e61d515d9cdb32f4d13b7270e45f1d4e
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-12-23 11:14:43 +0700

    fix env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -27,7 +27,6 @@ import (
 // @title CSCMS Storage
 // @version 1.0
 // @description This is documentation for CSCMS Storage API
-
 func main() {
 	logger := hclog.Default()
 
@@ -142,4 +141,27 @@ func main() {
 	if err != nil {
 		log.Fatalf("unable to start server on %s: %v", appENVs.Port, err)
 	}
+}
+
+type ApplicationEnvironmentVariable struct {
+	MasterKey                        string `env:"MASTER_KEY"`
+	FileStoragePath                  string `env:"STORAGE_PATH"`
+	FileStoreMaxDuration             int    `env:"STORE_DURATION" envDefault:"30"`
+	AzureBlobStorageConnectionString string `env:"AZSTORAGE_CONNECTION_STRING"`
+	AzureBlobStorageContainerName    string `env:"AZSTORAGE_CONTAINER_NAME"`
+	Port                             string `env:"PORT"`
+	DB                               DatabaseEnvironmentVariable
+	OauthGitHubClientSecret          string `env:"GITHUB_OAUTH_CLIENT_ID"`
+	OauthGitHubSecretKey             string `env:"GITHUB_OAUTH_SECRET_KEY"`
+	OAuthGoogleClientSecret          string `env:"GOOGLE_OAUTH_CLIENT_ID"`
+	OAuthGoogleSecretKey             string `env:"GOOGLE_OAUTH_SECRET_KEY"`
+	Entrypoint                       string `env:"ENTRYPOINT"`
+}
+
+type DatabaseEnvironmentVariable struct {
+	Username     string `env:"DB_USERNAME"`
+	Password     string `env:"DB_PASSWORD"`
+	Host         string `env:"DB_HOST"`
+	Port         string `env:"DB_PORT"`
+	DatabaseName string `env:"DB_DATABASE"`
 }

33f4df9099d7891ef3993227b72310134bffded5
GGolfz <wisarutgolf11@gmail.com>
2021-12-30 19:54:25 +0700

    add: call auth api :sparkles:

--- client/src/components/auth/SocialAuthButton.js
+++ client/src/components/auth/SocialAuthButton.js
@@ -1,35 +1,9 @@
 import Icon from '../util/Icon'
 import styles from '../../styles/auth/SocialAuth.module.css'
 import { toTitleCase } from '../../utils/formatText'
-import { useSetRecoilState } from 'recoil'
-import { authState } from '../../store/auth'
 const SocialAuthButton = ({ mode, platform }) => {
-	const setAuth = useSetRecoilState(authState)
 	const handleAction = (mode, platform) => {
-		switch (mode) {
-			case 'login':
-				setAuth({
-					isAuthenticated: true,
-					user: {
-						name: 'Wagyu',
-						image: 'https://tmp.cscms.me/9nwmgr',
-						email: 'wagyu@wagyu.com'
-					}
-				})
-				break
-			case 'signup':
-				setAuth({
-					isAuthenticated: true,
-					user: {
-						name: 'Momo',
-						image: 'https://tmp.cscms.me/9nwmgr',
-						email: 'momo@momo.com'
-					}
-				})
-				break
-			default:
-				return null
-		}
+		window.open(`https://storage.cscms.me/auth/${platform.toLowerCase()}`)
 	}
 	return (
 		<div

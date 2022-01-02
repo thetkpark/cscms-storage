f14888e8a95cae4ab9b045d7feba78382f335120
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-01-02 21:56:16 +0700

    ADD test on GORM datastore (#27)
    * use sqlite for test db
    
    * finish user_test.go
    
    * add custom provider
    
    * remove userRow
    
    * rename function
    
    * add createGormDB function
    
    * add file_test.go
    
    * add UpdateToken function
    
    * use UpdateToken function
    
    * use []model.File rather than point
    
    * compare len of ownfile
    
    * add image_test.go
    
    * fix ownfile typo
    
    * shorten the logic
    
    * add create user test
    
    * move helper functions to test_helper.go
    
    * use destroyTestGormDB function
    
    * add testing create GormDataStore function

---
+++ data/image_test.go
@@ -0,0 +1,102 @@
+package data
+
+import (
+	"github.com/go-test/deep"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"github.com/thetkpark/cscms-temp-storage/data/model"
+	"gorm.io/gorm"
+	"testing"
+)
+
+type GormImageDataStoreTestSuite struct {
+	suite.Suite
+	db        *gorm.DB
+	store     *GormImageDataStore
+	user      *model.User
+	image     *model.Image
+	ownImages []model.Image
+}
+
+func TestNewGormImageDataStore(t *testing.T) {
+	db, err := createTestGormDB()
+	require.NoError(t, err)
+	store, err := NewGormImageDataStore(db)
+	require.NoError(t, err)
+	require.NotNil(t, store)
+
+	require.NoError(t, db.Create(createTestImage(0)).Error)
+	require.NoError(t, destroyTestGormDB())
+}
+
+func TestGormImageDataStore(t *testing.T) {
+	suite.Run(t, new(GormImageDataStoreTestSuite))
+}
+
+func (s *GormImageDataStoreTestSuite) SetupTest() {
+	gormDB, err := createTestGormDB()
+	require.NoError(s.T(), err)
+	s.db = gormDB
+
+	require.NoError(s.T(), gormDB.AutoMigrate(&model.Image{}, &model.User{}))
+
+	s.store = &GormImageDataStore{db: gormDB}
+	require.NoError(s.T(), err)
+
+	s.user = createTestUser("github")
+	s.image = createTestImage(0)
+	s.ownImages = []model.Image{
+		*createTestImage(s.user.ID),
+		*createTestImage(s.user.ID),
+		*createTestImage(s.user.ID),
+	}
+	require.NoError(s.T(), s.db.Create(s.user).Error)
+	require.NoError(s.T(), s.db.Create(s.image).Error)
+	require.NoError(s.T(), s.db.Create(&s.ownImages).Error)
+}
+
+func (s *GormImageDataStoreTestSuite) AfterTest(_, _ string) {
+	require.NoError(s.T(), destroyTestGormDB())
+}
+
+func (s *GormImageDataStoreTestSuite) TestCreate() {
+	newImage := createTestImage(0)
+	require.NoError(s.T(), s.store.Create(newImage))
+
+	var queryImage model.Image
+	require.NoError(s.T(), s.db.Where(newImage).First(&queryImage).Error)
+	require.Nil(s.T(), deep.Equal(&queryImage, newImage))
+}
+
+func (s *GormImageDataStoreTestSuite) TestFindByID() {
+	queryImage, err := s.store.FindByID(s.image.ID)
+	require.NoError(s.T(), err)
+	require.Nil(s.T(), deep.Equal(queryImage, s.image))
+}
+
+func (s *GormImageDataStoreTestSuite) TestFindByIDNotFound() {
+	newImg := createTestImage(0)
+	queryImage, err := s.store.FindByID(newImg.ID)
+	require.NoError(s.T(), err)
+	require.Nil(s.T(), queryImage)
+}
+
+func (s *GormImageDataStoreTestSuite) TestFindByUserID() {
+	images, err := s.store.FindByUserID(s.user.ID)
+	require.NoError(s.T(), err)
+	require.Len(s.T(), *images, len(s.ownImages))
+}
+
+func (s *GormImageDataStoreTestSuite) TestFindByUserIDEmpty() {
+	newUser := createTestUser("google")
+	images, err := s.store.FindByUserID(newUser.ID)
+	require.NoError(s.T(), err)
+	require.Len(s.T(), *images, 0)
+}
+
+func (s *GormImageDataStoreTestSuite) TestDeleteByID() {
+	require.NoError(s.T(), s.store.DeleteByID(s.image.ID))
+
+	var queryImage model.Image
+	require.ErrorIs(s.T(), s.db.Where(s.image).First(&queryImage).Error, gorm.ErrRecordNotFound)
+}
---
+++ data/user_test.go
@@ -0,0 +1,122 @@
+package data
+
+import (
+	"github.com/bxcodec/faker/v3"
+	"github.com/go-test/deep"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"github.com/thetkpark/cscms-temp-storage/data/model"
+	"gorm.io/gorm"
+	"testing"
+)
+
+type GormUserDataStoreTestSuite struct {
+	suite.Suite
+	db    *gorm.DB
+	store *GormUserDataStore
+	user  *model.User
+}
+
+func TestNewGormUserDataStore(t *testing.T) {
+	db, err := createTestGormDB()
+	require.NoError(t, err)
+	store, err := NewGormUserDataStore(db)
+	require.NoError(t, err)
+	require.NotNil(t, store)
+
+	require.NoError(t, db.Create(createTestUser("github")).Error)
+	require.NoError(t, destroyTestGormDB())
+}
+
+func TestGormUserDataStore(t *testing.T) {
+	suite.Run(t, new(GormUserDataStoreTestSuite))
+}
+
+func (s *GormUserDataStoreTestSuite) SetupTest() {
+	gormDB, err := createTestGormDB()
+	require.NoError(s.T(), err)
+	s.db = gormDB
+
+	require.NoError(s.T(), gormDB.AutoMigrate(&model.User{}))
+
+	s.store = &GormUserDataStore{db: gormDB}
+	require.NoError(s.T(), err)
+
+	s.user = createTestUser("github")
+	require.NoError(s.T(), gormDB.Create(s.user).Error)
+	require.NoError(s.T(), gormDB.Create(createTestUser("github")).Error)
+	require.NoError(s.T(), gormDB.Create(createTestUser("google")).Error)
+}
+
+func (s *GormUserDataStoreTestSuite) AfterTest(_, _ string) {
+	require.NoError(s.T(), destroyTestGormDB())
+}
+
+func (s *GormUserDataStoreTestSuite) TestCreate() {
+	newUser := createTestUser("google")
+	user, err := s.store.Create(newUser.Email, newUser.Username, newUser.Provider, newUser.AvatarURL)
+	require.NoError(s.T(), err)
+
+	var queryUser model.User
+	require.NoError(s.T(), s.db.Where(user).First(&queryUser).Error)
+	require.Nil(s.T(), deep.Equal(user, &queryUser))
+}
+
+func (s *GormUserDataStoreTestSuite) TestFoundByID() {
+	foundUser, err := s.store.FindById(s.user.ID)
+	require.NoError(s.T(), err)
+	require.Nil(s.T(), deep.Equal(foundUser, s.user))
+}
+
+func (s *GormUserDataStoreTestSuite) TestNotFoundByID() {
+	newUser := createTestUser("github")
+	foundUser, err := s.store.FindById(newUser.ID)
+	require.NoError(s.T(), err)
+	require.Nil(s.T(), foundUser)
+}
+
+func (s *GormUserDataStoreTestSuite) TestFoundByProviderAndEmail() {
+	foundUser, err := s.store.FindByProviderAndEmail(s.user.Provider, s.user.Email)
+	require.NoError(s.T(), err)
+	require.Nil(s.T(), deep.Equal(foundUser, s.user))
+}
+
+func (s *GormUserDataStoreTestSuite) TestNotFoundByProviderAndEmail() {
+	newUser := createTestUser("github")
+	foundUser, err := s.store.FindByProviderAndEmail(newUser.Provider, newUser.Email)
+	require.NoError(s.T(), err)
+	require.Nil(s.T(), foundUser)
+}
+
+func (s *GormUserDataStoreTestSuite) TestFoundByAPIKey() {
+	foundUser, err := s.store.FindByAPIKey(s.user.APIKey)
+	require.NoError(s.T(), err)
+	require.Nil(s.T(), deep.Equal(foundUser, s.user))
+}
+
+func (s *GormUserDataStoreTestSuite) TestNotFoundByAPIKey() {
+	newUser := createTestUser("github")
+	foundUser, err := s.store.FindByAPIKey(newUser.APIKey)
+	require.NoError(s.T(), err)
+	require.Nil(s.T(), foundUser)
+}
+
+func (s *GormUserDataStoreTestSuite) TestUpdateAPIKey() {
+	newApiKey := faker.UUIDDigit()
+	require.NoError(s.T(), s.store.UpdateAPIKey(s.user.ID, newApiKey))
+
+	queryUser := &model.User{}
+	err := s.db.Where(&model.User{APIKey: newApiKey}).First(queryUser).Error
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), queryUser.ID, s.user.ID)
+}
+
+func (s *GormUserDataStoreTestSuite) TestUpdateAPIKeyOnNotFoundUser() {
+	newUser := createTestUser("github")
+	newApiKey := faker.UUIDDigit()
+	require.NoError(s.T(), s.store.UpdateAPIKey(newUser.ID, newApiKey))
+
+	queryUser := &model.User{}
+	err := s.db.Where(&model.User{APIKey: newApiKey}).First(queryUser).Error
+	require.ErrorIs(s.T(), err, gorm.ErrRecordNotFound)
+}

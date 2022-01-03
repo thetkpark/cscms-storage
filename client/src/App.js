57dc5821de034ea4e5d6be153f5e06a4ef9f4bd5
GGolfz <wisarutgolf11@gmail.com>
2022-01-03 15:29:33 +0700

    clean: project :fire:

--- client/src/App.js
+++ client/src/App.js
@@ -1,125 +1,252 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import FormData from 'form-data'
+import { useState, useEffect, Fragment } from 'react'
 import ReactGA from 'react-ga'
-import { TextField, Button, Text, Slider } from '@adobe/react-spectrum'
-import UploadIcon from '@spectrum-icons/workflow/UploadToCloudOutline'
-import Dropzone from './components/Dropzone'
+import Navbar from './components/layout/Navbar'
+import Sidebar from './components/layout/Sidebar'
+import AuthForm from './components/auth/AuthForm'
 import styles from './styles/App.module.css'
-import FileDataModal from './components/Modal'
-import ErrorDialog from './components/ErrorDialog'
-
+import { Dialog } from '@material-ui/core'
+import UploadContainer from './components/upload/UploadContainer'
+import { useRecoilState } from 'recoil'
+import { authState } from './store/auth'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+import FileList from './components/file/FileList'
+import UserProfile from './components/auth/UserProfile'
+import { formatFileSize } from './utils/formatFileSize'
+import { formatDate } from './utils/formatText'
 function App() {
+	const [route, setRoute] = useState('file')
+	const [auth, setAuth] = useRecoilState(authState)
+	const [dialog, setDialog] = useState(null)
 	const [progress, setProgress] = useState(-1)
-	const [selectedFile, setSelectedFile] = useState(null)
-	const [slug, setSlug] = useState('')
-	const [fileData, setFileData] = useState(undefined)
-	const [error, setError] = useState('')
-	const [showModal, setShowModal] = useState(false)
-	const [storeDuration, setStoreDuration] = useState(7)
-
+	const [error, setError] = useState(null)
 	useEffect(() => {
 		ReactGA.initialize('G-S7NPY62JTS')
 		ReactGA.pageview(window.location.pathname)
 	}, [])
+	useEffect(() => {
+		axios
+			.get('https://storage.cscms.me/auth/user')
+			.then(res => {
+				setAuth({
+					isAuthenticated: true,
+					user: {
+						name: res.data.username,
+						image: res.data.avatar_url,
+						email: res.data.email
+					}
+				})
+			})
+			.catch(() => {
+				setAuth({
+					isAuthenticated: false,
+					user: null
+				})
+			})
+	}, [setAuth])
+	useEffect(() => {
+		if (auth.isAuthenticated) {
+			setDialog(null)
+		}
+	}, [auth])
 
-	const onDrop = (acceptedFiles, rejectedFiles) => {
-		if (acceptedFiles.length === 1) {
-			setError('')
-			setSelectedFile(acceptedFiles[0])
-		} else {
-			if (rejectedFiles[0].errors[0].code === 'too-many-files') {
-				setError('Too many files. You can only upload one file at a time')
-			} else if (rejectedFiles[0].errors[0].code === 'file-too-large') {
-				setError('File too big. The size limit is 100MB')
-			} else setError('File not accepted')
+	useEffect(() => {
+		if (error != null) {
+			Swal.fire({
+				title: 'An error occured',
+				text: error,
+				icon: 'error',
+				confirmButtonText: 'Ok'
+			})
+			setError(null)
+		}
+	}, [error])
+
+	const handleAction = action => {
+		switch (action) {
+			case 'signup':
+				setDialog('signup')
+				break
+			case 'login':
+				setDialog('login')
+				break
+			case 'logout':
+				axios
+					.get('https://storage.cscms.me/auth/logout')
+					.then(() => {
+						setAuth(false)
+					})
+					.catch(err => {
+						setError(err.response.data.message)
+					})
+				break
+			default:
+				break
 		}
 	}
-
-	const handleSubmission = async event => {
-		event.preventDefault()
-		if (!selectedFile) {
-			setError('Please select a file')
-			return
+	const handleChangeRoute = newRoute => {
+		if (newRoute === route) return
+		setRoute(newRoute)
+	}
+	const handleUpload = data => {
+		try {
+			if (route === 'file') {
+				handleUploadFile(data)
+			} else if (route === 'image') {
+				handleUploadImage(data)
+			}
+		} catch (err) {
+			throw err
 		}
+	}
+	const handleUploadFile = async ({ selectedFile, slug, duration, clearFile }) => {
 		const formdata = new FormData()
 		formdata.append('file', selectedFile)
 
 		try {
-			const res = await axios.post('/api/file', formdata, {
+			const res = await axios.post('https://storage.cscms.me/api/file', formdata, {
 				onUploadProgress: progressEvent => {
 					const uploadPercent = Math.round(
 						(progressEvent.loaded / progressEvent.total) * 100
 					)
 					setProgress(uploadPercent)
 				},
-				params: { slug, duration: storeDuration }
+				params: { slug, duration }
 			})
-
-			setFileData(res.data)
-			setShowModal(true)
+			setProgress(-1)
+			clearFile()
+			const { token, filename, file_size, expired_at } = res.data
+			var html =
+				'<div style="text-align:left">Download URL: https://storage.cscms.me/' +
+				token +
+				'<br>File name: ' +
+				filename +
+				'<br>File size: ' +
+				formatFileSize(file_size) +
+				'<br>Valid Though: ' +
+				formatDate(expired_at) +
+				'</div>'
+			Swal.fire({
+				title: 'Upload File Success',
+				icon: 'success',
+				html,
+				showCancelButton: true,
+				confirmButtonText: 'Copy URL',
+				cancelButtonText: 'Close'
+			}).then(result => {
+				if (result.isConfirmed) {
+					var copyText = document.createElement('input')
+					copyText.setAttribute('value', 'https://storage.cscms.me/' + token)
+					document.body.appendChild(copyText)
+					copyText.select()
+					copyText.setSelectionRange(0, 99999)
+					navigator.clipboard.writeText(copyText.value)
+					document.body.removeChild(copyText)
+				}
+			})
 			ReactGA.event({
 				category: 'file',
 				action: 'Upload file',
 				value: selectedFile.size
 			})
 		} catch (err) {
-			console.log(err)
-			setError(err.response.data.message)
+			throw err
 		}
 	}
+	const handleUploadImage = async ({ selectedFile, clearFile }) => {
+		const formdata = new FormData()
+		formdata.append('image', selectedFile)
 
-	const closeDialogAndReset = () => {
-		setShowModal(false)
-		setProgress(-1)
-		setSelectedFile(null)
-		setSlug('')
-		setFileData(undefined)
-		setError('')
+		try {
+			const res = await axios.post('https://storage.cscms.me/api/image', formdata, {
+				onUploadProgress: progressEvent => {
+					const uploadPercent = Math.round(
+						(progressEvent.loaded / progressEvent.total) * 100
+					)
+					setProgress(uploadPercent)
+				}
+			})
+			setProgress(-1)
+			clearFile()
+			const { file_path, original_filename, file_size } = res.data
+			var html =
+				'<div style="text-align:left">URL: https://img.cscms.me/' +
+				file_path +
+				'<br>File name: ' +
+				original_filename +
+				'<br>File size: ' +
+				formatFileSize(file_size) +
+				'</div>'
+			Swal.fire({
+				title: 'Upload Image Success',
+				icon: 'success',
+				html,
+				width: '600px',
+				showCancelButton: true,
+				confirmButtonText: 'Copy URL',
+				cancelButtonText: 'Close'
+			}).then(result => {
+				if (result.isConfirmed) {
+					var copyText = document.createElement('input')
+					copyText.setAttribute('value', 'https://img.cscms.me/' + file_path)
+					document.body.appendChild(copyText)
+					copyText.select()
+					copyText.setSelectionRange(0, 99999)
+					navigator.clipboard.writeText(copyText.value)
+					document.body.removeChild(copyText)
+				}
+			})
+			ReactGA.event({
+				category: 'image',
+				action: 'Upload Image',
+				value: selectedFile.size
+			})
+		} catch (err) {
+			throw err
+		}
 	}
-
+	const renderScreen = () => {
+		switch (route) {
+			case 'file':
+			case 'image':
+				return (
+					<UploadContainer
+						type={route}
+						handleUpload={handleUpload}
+						setError={setError}
+						progress={progress}
+					/>
+				)
+			case 'myfile':
+				if (auth.isAuthenticated) return <FileList setError={setError} />
+				setRoute('file')
+				break
+			default:
+				setRoute('file')
+				break
+		}
+	}
 	return (
-		<div className={styles.App}>
-			<div className={styles.AppContainer}>
-				{/* <h1 className={styles.Heading}>CSCMS Temp Storage</h1> */}
-				<Dropzone
-					onDrop={onDrop}
-					selectedFilename={selectedFile ? selectedFile.name : ''}
-					progress={progress}
-					error={error}
-				/>
-				<div className={styles.FormContainer}>
-					<form className={styles.Form} onSubmit={handleSubmission}>
-						<TextField
-							label="Custom slug for accessing the file (Optional)"
-							placeholder="Slug"
-							value={slug}
-							onChange={e => setSlug(e)}
-							width="300px"
-							type="text"
-							inputMode="text"
-						/>
-						<Slider
-							label="Store Duration (Days)"
-							value={storeDuration}
-							onChange={setStoreDuration}
-							minValue={1}
-							maxValue={30}
-						/>
-						<Button variant="primary" type="submit">
-							<UploadIcon />
-							<Text>Upload</Text>
-						</Button>
-					</form>
+		<Fragment>
+			<div className={styles.App}>
+				<div>
+					<Navbar auth={auth.isAuthenticated} handleAction={handleAction} />
+					<div className={styles.ContentWrapper}>
+						{auth.isAuthenticated ? (
+							<UserProfile user={auth.user} handleChangeRoute={handleChangeRoute} />
+						) : null}
+
+						{renderScreen()}
+					</div>
+					<Sidebar currentRoute={route} handleChangeRoute={handleChangeRoute} />
+					{!auth.isAuthenticated && dialog ? (
+						<Dialog open={dialog !== null} onClose={() => setDialog(null)}>
+							<AuthForm mode={dialog} changeMode={mode => setDialog(mode)} />
+						</Dialog>
+					) : null}
 				</div>
-				<FileDataModal
-					show={showModal}
-					closeDialog={closeDialogAndReset}
-					fileData={fileData}
-				/>
-				{error && <ErrorDialog errorMessage={error} closeDialog={() => setError('')} />}
 			</div>
-		</div>
+		</Fragment>
 	)
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
-import App from './NewApp'
+import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { RecoilRoot } from 'recoil'
 ReactDOM.render(

7e8bc751208be1b3e1fb291d5c93df1bbd4b0699
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-01-07 14:17:59 +0700

    add most of manager mock

--- handlers/test_helper.go
+++ handlers/test_helper.go
@@ -30,6 +30,74 @@ func (m *MockImageDataStore) DeleteByID(imageId uint) error {
 	return args.Error(0)
 }
 
+type MockFileDataStore struct {
+	mock.Mock
+}
+
+func (m *MockFileDataStore) Create(file *model.File) error {
+	args := m.Called(file)
+	return args.Error(0)
+}
+
+func (m *MockFileDataStore) FindByID(fileID string) (*model.File, error) {
+	args := m.Called(fileID)
+	return args.Get(0).(*model.File), args.Error(1)
+}
+
+func (m *MockFileDataStore) FindByToken(token string) (*model.File, error) {
+	args := m.Called(token)
+	return args.Get(0).(*model.File), args.Error(1)
+}
+
+func (m *MockFileDataStore) IncreaseVisited(id string) error {
+	args := m.Called(id)
+	return args.Error(0)
+}
+
+func (m *MockFileDataStore) FindByUserID(userId uint) (*[]model.File, error) {
+	args := m.Called(userId)
+	return args.Get(0).(*[]model.File), args.Error(1)
+}
+
+func (m *MockFileDataStore) DeleteByID(fileId string) error {
+	args := m.Called(fileId)
+	return args.Error(0)
+}
+
+func (m *MockFileDataStore) UpdateToken(fileID string, newToken string) error {
+	args := m.Called(fileID, newToken)
+	return args.Error(0)
+}
+
+type MockDiskStorageManager struct {
+	mock.Mock
+}
+
+func (m *MockDiskStorageManager) OpenFile(fileName string) (io.Reader, error) {
+	args := m.Called(fileName)
+	return args.Get(0).(io.Reader), args.Error(1)
+}
+
+func (m *MockDiskStorageManager) WriteToNewFile(fileName string, reader io.Reader) error {
+	args := m.Called(fileName, reader)
+	return args.Error(0)
+}
+
+func (m *MockDiskStorageManager) Exist(fileName string) (bool, error) {
+	args := m.Called(fileName)
+	return args.Bool(0), args.Error(1)
+}
+
+func (m *MockDiskStorageManager) ListFiles() ([]string, error) {
+	args := m.Called()
+	return args.Get(0).([]string), args.Error(1)
+}
+
+func (m *MockDiskStorageManager) DeleteFile(fileName string) error {
+	args := m.Called(fileName)
+	return args.Error(0)
+}
+
 type MockImageStorageManager struct {
 	mock.Mock
 }
@@ -80,4 +148,18 @@ func (m *MockTokenManager) GenerateImageToken() (string, error) {
 func (m *MockTokenManager) GenerateAPIToken() (string, error) {
 	args := m.Called()
 	return args.String(0), args.Error(1)
+}
+
+type MockEncryptionManager struct {
+	mock.Mock
+}
+
+func (m *MockEncryptionManager) Encrypt(input io.Reader) (io.Reader, string, error) {
+	args := m.Called(input)
+	return args.Get(0).(io.Reader), args.String(1), args.Error(2)
+}
+
+func (m *MockEncryptionManager) Decrypt(input io.Reader, nonceString string) (io.Reader, error) {
+	args := m.Called(input, nonceString)
+	return args.Get(0).(io.Reader), args.Error(1)
 }
